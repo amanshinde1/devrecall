86591e1bc5e00c6219af7f6d057033e9e18a4e93
amanshinde1 <amanshinde316@gmail.com>
2026-01-08 15:44:52 +0530

    V2: add serializers for core models

---
+++ backend/core/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from .models import Topic, Pattern, Problem
+
+
+class TopicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Topic
+        fields = [
+            'id',
+            'name',
+            'created_at',
+        ]
+
+
+class PatternSerializer(serializers.ModelSerializer):
+    topic = serializers.StringRelatedField()
+
+    class Meta:
+        model = Pattern
+        fields = [
+            'id',
+            'name',
+            'topic',
+            'description',
+            'created_at',
+        ]
+
+
+class ProblemSerializer(serializers.ModelSerializer):
+    pattern = serializers.StringRelatedField()
+
+    class Meta:
+        model = Problem
+        fields = [
+            'id',
+            'title',
+            'pattern',
+            'difficulty',
+            'external_link',
+            'created_at',
+        ]
